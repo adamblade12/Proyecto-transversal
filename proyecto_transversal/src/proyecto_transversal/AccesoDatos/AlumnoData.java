65a454fdb1a19352c3615c7868cb4bcfc348ed97
Escalante Pablo <141648314+adamblade12@users.noreply.github.com>
2023-09-07 12:28:36 -0300

    pongo lo basico del proyecto

---
+++ proyecto_transversal/src/proyecto_transversal/AccesoDatos/AlumnoData.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_transversal.AccesoDatos;
+
+/**
+ *
+ * @author pablo
+ */
+public class AlumnoData {
+    
+}
---
+++ proyecto_transversal/src/proyecto_transversal/Entidades/Alumno.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_transversal.Entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author pablo
+ */
+public class Alumno {
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    LocalDate fechaNac;
+    boolean activo;
+
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+
+    public Alumno(String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+    
+    public Alumno(){}
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
+}
